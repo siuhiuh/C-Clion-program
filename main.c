906075773c7601e0fb4cbff21b69cbf456c6421b
siuhiuh <1435372493@qq.com>
2025-10-31 16:50:52 +0800

    C语言第13天（25/10/31）

--- main.c
+++ main.c
@@ -1,93 +1,109 @@
-//C语言第12天（25/10/29）
+//C语言第13天（25/10/31）
+#include <stdio.h>
+
+int main(void) {
+
+        // 用方式一定义
+        int arr[2][3] = {
+                [0] = {[0] = 1, [1] = 2, [2] = 3},
+                [1] = {[0] = 11, [1] = 22, [2] = 33}
+        };
+
+        // 遍历数组
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                printf("%d ", arr[i][j]);
+            }
+            printf("\n");
+        }
+
+        return 0;
+
+}
 
 /**
-* 一、没有类型的指针：
-void* p不表示任何类型，但可以放入任意的地址
-//1、定义两个变量
-int a = 10 ;
-short b = 20 ;
-//指针变量（类型对应）
-int* p1 = &a ;//指明地址
-int* p2 = &b ;
-printf("%d\n" , *p1) ;
-printf("%d\n" , *p2) ;
-//无类型指针的使用
-//可等于一个指针（接受他的内存地址）
-void* p3 = p1 ;
-void* p4 = p2 ;
-//但他无法获取指针中的具体数值，也无法进行加减法（报错）
-//printf("%d" , *p3) ;
-//printf("%p" , p3+1) ;
-
-
-//运用场景：调用函数交换数据（好处是可以换算各种类型的数据，不用反复改数据类型）
-#include <stdio.h>
-
-void swap(void* p1 , void* p2 , int len) ;
-int main(void) {
-    long long c = 100L ;
-    long long d = 200L ;
-    swap(&c , &d , 8);//传送地址，void可接受所有数据类型
-    printf("c=%d d=%d" , c , d);
-}
-
-
-
-void swap(void* p1 , void* p2 , int len)
-{
-//把void类型换成char
-    char* pc1 = p1 ;
-    char* pc2 = p2 ;
-//一个字节一个字节的交换，就可以一个不漏的交换内存地址，从而达到交换数据
-    char temp = 0 ;//定一个中间变量
-    for (int i = 0 ; i  < len ; i++)
-    {
-        temp = *pc1 ;
-        *pc1 = *pc2 ;
-        *pc2 = temp ;
-        pc1++ ;
-        pc2++ ; //(内存地址加一个步长)
-    }
-}
-二、二级指针和多级指针
-指针也是一个变量，他本身也占用对应的地址
-数据类型与指向空间中的数据类型一致
-1、二级指针的格式：
-int** 指针名//因为一级是int*
-#include <stdio.h>
+* 一、数组指针的细节
+ * 1、arr参与计算的时候 会退化为第一个元素的首地址（指针的内存）
+ * 2、但有特殊情况
+ *#include <stdio.h>
 
 int main(void) {
-    int a = 10;
-    int b = 20;
-
-    int *p = &a;
-    int **pp = &p;//二级指针指向一级的地址
-//作用一：利用二级指针修改一级指针中的地址
-    *pp = &b;//一次解运算符，对pp来说是获取地址（已知pp的地址原先是二级指针）
-//作用二：利用二级指针获取变量中记录的数据
-    printf("%p\n", &a);
-    printf("%p\n", &b);
-    printf("%p\n", p);
-    printf("%p\n", *pp);
-    printf("%d\n", *p);
-    printf("%d\n", **pp);//解引用运算符解两次得到对应值
-}
-
-三、数组指针
-1、练习：利用指针遍历数组
-#include <stdio.h>
-
-int main(void) {
-    //指针遍历数组
     //定义一个数组
-    int arr[] = {1 , 3, 5 , 7 , 9};
-    int len = sizeof(arr) / sizeof(arr[0]) ;
-    //用指针指向首地址然后遍历
-    int* p = &arr[0] ;//忘记加地址符了
-    for(int i = 0 ; i < len ; i++)
-    {
-        printf("%d  " , *p);
-        p++ ;//每次步长移动一个（移动到下一个数据的首地址）
-    }
+    int arr[] = {1, 3 , 5, 7 , 9} ;
+    //当arr计算长度时是一个整体
+    int len = sizeof(arr) / sizeof(int) ;
+    printf("%d\n" , len) ;
+    //当arr单独出现时是一个整体
+    printf("%zu\n" , sizeof(arr)) ;
+    //&arr获取地址时，不会退化，即使是在运算中
+    printf("%p\n" , arr) ;
+    printf("%p\n" , &arr) ;//两者都是首地址
+    printf("%p\n" , arr + 1) ; //一步跳跃对应的数据类型
+    printf("%p\n" , &arr + 1) ;//一步跳跃整体
+//学会用电脑calc计算十六进制跳跃了几步
+}
+*/
+
+/**
+* 二、用&arr遍历多维数组
+ * 1、什么是二维数组：小数组放在大数组中
+ * //方式1
+ * arr[m][n] =
+ * {
+ * {1,2,3,4},
+ * {5,6,7,8},
+ * };
+ * //方式2
+ * int arr1[4] = {1,2,3,4};
+ * int arr2[4] = {1,2,3,4};
+ * int* arr[2] = {arr1 , arr2} ;
+ *
+ *
+ *#include <stdio.h>
+
+int main(void) {
+
+        // 用方式一定义
+        int arr[2][3] = {
+                [0] = {[0] = 1, [1] = 2, [2] = 3},
+                [1] = {[0] = 11, [1] = 22, [2] = 33}
+        };
+
+        // 遍历数组
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                printf("%d ", arr[i][j]);
+            }
+            printf("\n");
+        }
+
+        return 0;
+
+}
+*/
+
+/**
+ * 三、长度不同时通过索引遍历
+* #include <stdio.h>
+
+int main(void) {
+  //当长度不一样时，使用方法二
+  int arr1[3] = {1 , 2, 3} ;
+  int arr2[4] = {1 , 2, 3 , 4} ;
+  int len1 = sizeof(arr1) / sizeof(int) ;
+  int len2 = sizeof(arr2) / sizeof(int) ;
+  //将len设为数组
+  int len[2] = {len1 , len2 };//这里面包含的整数
+  //二维数组定义
+  int* arr[2] = {arr1 , arr2} ;//不再是整数，而是存的指针指向各个以为数组的首地址
+  //循环索引遍历
+  for(int i = 0 ; i < 2 ; i++)//记得确认i的范围
+  {
+      for(int j = 0 ; j < len[i] ; j++)
+      {
+          printf("%d " , arr[i][j]) ;
+      }
+      printf("\n") ;
+  }
 }
 */
