406abcab64b24bcc831223dc99f2c45c3a3c7ed9
siuhiuh <1435372493@qq.com>
2025-11-15 20:34:59 +0800

    C语言第20天（25/11/15）

--- main.c
+++ main.c
@@ -1,172 +1,130 @@
-//C语言第19天（25/11/13）
+//C语言第20天（25/11/15）
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-struct Message
-        {
- char phone[12] ;//十一位多加一位
- char mail[100] ;
-};
 
-struct student
+struct spot
 {
-    char name[100] ;
-    int age ;
-    double height ;
-    struct Message msg ;//取名字直接在这里取
+   int a ;//(4)0,1,2,3
+   char b ;//(1)4
+   char c ;//(1)5 (6,7)
+   double d ;//(8) 8
+   //0,1,2,3
+   //4,5,6,7
+   //此时并未对齐
+   //8，（9，10，11）
+   //（12，13，14，15）。为了能被最大的数据类型整除
+   //共16位
 };
-
 
 int main(void) {
-    //1、定义一个学生
-    struct  student stu1 ;
-    //2、赋值
-    strcpy(stu1.name , "zhangsan") ;
-    stu1.age = 19 ;
-    stu1.height = 1.48 ;
-    //重点，给嵌套结构体赋值(逐层解开)
-    strcpy(stu1.msg.phone , "13112345678") ;
-    strcpy(stu1.msg.mail , "13112345678@qq.com") ;
-    printf("以下是该同学的一些信息：\n") ;
-    printf("姓名为：%s\n" , stu1.name) ;
-    printf("年龄为：%d\n", stu1.age) ;
-    printf("身高为：%f\n" , stu1.height) ;
-    printf("姓名为：%s\n" , stu1.msg.phone ) ;
-    printf("姓名为：%s\n" , stu1.msg.mail) ;
-
-    //简便方法：一次性赋值
-    struct  student stu2 = {"zhangsan" , 43 , 1.49 , {"13212345679" , "123@qq.com"}};
-    //嵌套的一块再加一个大括号，其中非字符串类型不用加引号
-    printf("以下是该同学的一些信息：\n") ;
-    printf("姓名为：%s\n" , stu2.name) ;
-    printf("年龄为：%d\n", stu2.age) ;
-    printf("身高为：%f\n" , stu2.height) ;
-    printf("姓名为：%s\n" , stu2.msg.phone ) ;
-    printf("姓名为：%s\n" , stu2.msg.mail) ;
-
-
+    //内存地址能被数据类型整除（数组的话就是首地址）
+    //一、非结构体的数据类型验证地址是否为其整数倍
+    int a ;
+    printf("%d\n" , &a) ;//%p是16进制进行打印，%d是十进制进行打印
+    //二、结构体验证它的字节计算方式
+    //定义一个结构体：
+    struct spot n ;
+    printf("%zu\n" , sizeof(n)) ;
 }
 
 
 
 /**
-* 一、结构体作为函数的参数：函数中可传递结构体
- * 1、传递数据
- * 2、传递地址
+* 一、结构体综合练习：投票选举
+ * 共ABCD个景点
+ *1、学生投票，用随机数模拟
+ * 2、如果多景点投票数量一致，A优先B ， B优先于C
  *
  * #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-typedef struct
+
+struct spot
 {
     char name[100] ;
-    int age ;
-}S;
-
-void method1(S st) ;//函数title放在结构体下方，否则识别不到
-void method2(S* p) ;
+    int count ;
+};
 
 int main(void) {
-    //1、定义一个学生
-    S stu ;
-    //2、给学生赋初始值
-    strcpy(stu.name , "aaa") ;//给字符串赋值要用字符串函数
-    stu.age = 0 ;//定义后直接调用就好了，不必引用结构体名称
-    //3、打印出当前的学生信息
-    printf("学生的姓名为：%s\n" , stu.name) ;
-    printf("学生的年龄为：%d\n" , stu.age) ;
-    //4、调用函数（尝试直接改变结构体内的值）
-    method1(stu) ;//把名称给他就行
-    //5、查看修改后的信息
-    printf("修改后学生姓名：%s\n" , stu.name) ;
-    printf("修改后学生年龄：%d\n" , stu.age) ;
+    //1、定义四个景点（结构体数组：相当于嵌套，可以同时容纳多个不同类型的数据）
+    struct spot arr[4] = {{'A' , 0} , {'B', 0} ,{'C' , 0} ,{'D' , 0} };
+    //可同时赋值，注意字符串赋值一定要加单引号
 
-    //将地址传递过去
-    method2(&stu) ;//仍然不需要用S别名
-    printf("修改后学生姓名：%s\n" , stu.name) ;
-    printf("修改后学生年龄：%d\n" , stu.age) ;
+    //2、80个学生进行随机投票（随机数、循环）
+    srand(time(NULL)) ;
+    //这里的srand为什么不写在循环内：因为rand已经写在循环内了，取多少个rand就会有多少次srand
+    for(int i = 0 ; i <= 80 ; i++)
+    {
+       //将rand变换为结构体数组的索引，需要一个新的变量
+       int number = rand() % 4 ;//这里只能取0~4了
+       //对应索引中的count就自动++
+       arr[number].count++ ;
 
-}
-//直接修改值会发现并未修改成功
-//因为method1相当于定义了一个新变量，改变的是st的值，而不是stu
-void method1(S st)//结构体名称+自定义名称
-{
-    printf("接收到的学生姓名为：%s\n" , st.name ) ;
-    printf("接收到的学生年龄为：%d\n" , st.age ) ;
-    //开始做修改(键盘)
-    printf("请输入修改后的学生姓名：") ;
-    fflush(stdout) ;
-    scanf("%s" , st.name) ;//name本身是数组，当参与运算时，直接退化为指向首位的指针
-    printf("请输入修改后的学生年龄：") ;
-    fflush(stdout) ;
-    scanf("%d" , &(st.age)) ;//这里的黄色警告不必担心
-}
-//定义一个指针
-void method2(S* p)
-{
-    printf("接收到的学生姓名为：%s\n" , (*p).name ) ;//要将结构体的地址解引用
-    printf("接收到的学生年龄为：%d\n" , (*p).age ) ;
-    //开始做修改(键盘)
-    printf("请输入修改后的学生姓名：") ;
-    fflush(stdout) ;
-    scanf("%s" , (*p).name) ;
-    printf("请输入修改后的学生年龄：") ;
-    fflush(stdout) ;
-    scanf("%d" , &((*p).age)) ;
+    }
+    //3、比较出这四个景点票数的最大值
+    //易忽略点：先设一个最大值，一般是数组中的第一个数据
+    int max = arr[0].count ;
+    for(int i = 1 ; i < 4 ; i++)
+    {
+        if(arr[i].count > max)//注意这里比较的是count
+        {
+            max = arr[i].count ;
+        }
+    }
+
+    //4、将最大值对应的景点找出来（有相同值时，只取第一个值）
+    //遍历数组
+    for(int i = 0 ; i <4 ; i++)
+    {
+        if(arr[i].count == max)
+        {
+            printf("最受欢迎的景点为：%s , 总计票数： %d\n" , arr[i].name , arr[i].count) ;
+        }
+    }
+//把所有数据遍历出来检查：
+    for(int i = 0 ; i <4 ; i++)
+    {
+        printf("%s ,%d\n" , arr[i].name , arr[i].count) ;
+    }
 
 }
 */
 
 /**
-* 二、结构体嵌套
- * 成员包含结构体
- * 联系方式包括了手机号和电子邮箱
+* 二、结构体内存对齐（结构体的内存分布）
+ * 1、只能放在自己类型整数倍的内存地址上，对于任何数据类型都是如此
+ *2、最后一个补位：结构体的总大小，是最大类型的整数倍
  *
- * include <stdio.h>
-#include <string.h>
+ * #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-struct Message
-        {
- char phone[12] ;//十一位多加一位
- char mail[100] ;
-};
 
-struct student
+struct spot
 {
-    char name[100] ;
-    int age ;
-    double height ;
-    struct Message msg ;//取名字直接在这里取
+   int a ;//(4)0,1,2,3
+   char b ;//(1)4
+   char c ;//(1)5 (6,7)
+   double d ;//(8) 8
+   //0,1,2,3
+   //4,5,6,7
+   //此时并未对齐
+   //8，（9，10，11）
+   //（12，13，14，15）。为了能被最大的数据类型整除
+   //共16位
 };
-
 
 int main(void) {
-    //1、定义一个学生
-    struct  student stu1 ;
-    //2、赋值
-    strcpy(stu1.name , "zhangsan") ;
-    stu1.age = 19 ;
-    stu1.height = 1.48 ;
-    //重点，给嵌套结构体赋值(逐层解开)
-    strcpy(stu1.msg.phone , "13112345678") ;
-    strcpy(stu1.msg.mail , "13112345678@qq.com") ;
-    printf("以下是该同学的一些信息：\n") ;
-    printf("姓名为：%s\n" , stu1.name) ;
-    printf("年龄为：%d\n", stu1.age) ;
-    printf("身高为：%f\n" , stu1.height) ;
-    printf("姓名为：%s\n" , stu1.msg.phone ) ;
-    printf("姓名为：%s\n" , stu1.msg.mail) ;
-
-    //简便方法：一次性赋值
-    struct  student stu2 = {"zhangsan" , 43 , 1.49 , {"13212345679" , "123@qq.com"}};
-    //嵌套的一块再加一个大括号，其中非字符串类型不用加引号
-    printf("以下是该同学的一些信息：\n") ;
-    printf("姓名为：%s\n" , stu2.name) ;
-    printf("年龄为：%d\n", stu2.age) ;
-    printf("身高为：%f\n" , stu2.height) ;
-    printf("姓名为：%s\n" , stu2.msg.phone ) ;
-    printf("姓名为：%s\n" , stu2.msg.mail) ;
-
-
+    //内存地址能被数据类型整除（数组的话就是首地址）
+    //一、非结构体的数据类型验证地址是否为其整数倍
+    int a ;
+    printf("%d\n" , &a) ;//%p是16进制进行打印，%d是十进制进行打印
+    //二、结构体验证它的字节计算方式
+    //定义一个结构体：
+    struct spot n ;
+    printf("%zu\n" , sizeof(n)) ;
 }
 */
